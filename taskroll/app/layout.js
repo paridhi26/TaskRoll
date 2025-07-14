b5b8d466e17989e7eaafff4873efe2721fec9d7c
paridhi <paridhi.goel@mail.utoronto.ca>
2025-07-14 16:44:23 -0400

    custom metadata for the head

--- taskroll/app/layout.js
+++ taskroll/app/layout.js
@@ -40,6 +40,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
